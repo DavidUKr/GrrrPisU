432a77daf68fc0961d4c0faf45011d068758dec0
Cristina <cristina.rodean123@gamil.com>
2023-04-28 13:37:16 +0300

    last commit before switching

--- src/main/java/UI/SceneLoaders/PopUpLoader.java
+++ src/main/java/UI/SceneLoaders/PopUpLoader.java
@@ -9,7 +9,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
