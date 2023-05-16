8964540585383e0d391e97287d57ebfd643cd82e
Cristina <cristina.rodean123@gamil.com>
2023-05-16 10:11:58 +0300

    Added get resolution and frame rate

---
+++ src/main/java/OpenGL/JOGLInterface.java
@@ -0,0 +1,9 @@
+package OpenGL;
+
+import com.jogamp.opengl.GLAutoDrawable;
+
+public interface JOGLInterface {
+    void getResolution(GLAutoDrawable drawable);
+
+    void getFrameRate(GLAutoDrawable drawable);
+}
