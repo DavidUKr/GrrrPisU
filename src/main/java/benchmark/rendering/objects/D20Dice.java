e9f32442a76c640fe6472e230596fd88443c76a7
MihaiUM2001 <mihai-alexandru.udrescu-milosav@student.upt.ro>
2023-05-18 15:25:38 +0300

    HUGE D20/DnD/Icosahedron update (further testing required tho)

--- src/main/java/benchmark/rendering/objects/D20Dice.java
+++ src/main/java/benchmark/rendering/objects/D20Dice.java
@@ -14,18 +14,46 @@ public class D20Dice implements IObject//DnD Dice
     ArrayList<Path2D> PATHS=new ArrayList<Path2D>();
 
     public D20Dice(){
-        Vertex vA = new Vertex(100, 100, 100);
-        Vertex vB = new Vertex(-100, -100, 100);
-        Vertex vC = new Vertex(-100, 100, -100);
-        Vertex vD = new Vertex(100, -100, -100);
+        double phi = (1 + Math.sqrt(5)) / 2;
 
-        TRIS.add(new Triangle(vA, vB, vC, Color.WHITE));
+        Vertex vA = new Vertex(-1, phi, 0);
+        Vertex vB = new Vertex(1, phi, 0);
+        Vertex vC = new Vertex(-1, -phi, 0);
+        Vertex vD = new Vertex(1, -phi, 0);
 
-        TRIS.add(new Triangle(vA, vB, vD, Color.RED));
+        Vertex vE = new Vertex(0, -1, phi);
+        Vertex vF = new Vertex(0, 1, phi);
+        Vertex vG = new Vertex(0, -1, -phi);
+        Vertex vH = new Vertex(0, 1, -phi);
 
-        TRIS.add(new Triangle(vC, vD, vA, Color.GREEN));
+        Vertex vI = new Vertex(phi, 0, -1);
+        Vertex vJ = new Vertex(phi, 0, 1);
+        Vertex vK = new Vertex(-phi, 0, -1);
+        Vertex vL = new Vertex(-phi, 0, 1);
 
-        TRIS.add(new Triangle(vC, vD, vB, Color.BLUE));
+        TRIS.add(new Triangle(vA, vB, vF, Color.RED));
+        TRIS.add(new Triangle(vB, vA, vE, Color.RED));
+        TRIS.add(new Triangle(vA, vF, vG, Color.RED));
+        TRIS.add(new Triangle(vA, vG, vC, Color.RED));
+        TRIS.add(new Triangle(vA, vC, vE, Color.RED));
+
+        TRIS.add(new Triangle(vD, vB, vE, Color.RED));
+        TRIS.add(new Triangle(vB, vD, vH, Color.RED));
+        TRIS.add(new Triangle(vB, vH, vF, Color.RED));
+        TRIS.add(new Triangle(vB, vF, vE, Color.RED));
+        TRIS.add(new Triangle(vC, vG, vL, Color.RED));
+
+        TRIS.add(new Triangle(vG, vC, vK, Color.RED));
+        TRIS.add(new Triangle(vG, vK, vD, Color.RED));
+        TRIS.add(new Triangle(vG, vD, vH, Color.RED));
+        TRIS.add(new Triangle(vG, vH, vL, Color.RED));
+        TRIS.add(new Triangle(vE, vC, vK, Color.RED));
+
+        TRIS.add(new Triangle(vC, vE, vI, Color.RED));
+        TRIS.add(new Triangle(vC, vI, vL, Color.RED));
+        TRIS.add(new Triangle(vC, vL, vK, Color.RED));
+        TRIS.add(new Triangle(vD, vH, vJ, Color.RED));
+        TRIS.add(new Triangle(vD, vJ, vK, Color.RED));
 
         for(Triangle t : TRIS)
         {
