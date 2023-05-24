92282aab9ec28f8a34545dface05f81afcc3859a
Cristina <cristina.rodean123@gamil.com>
2023-05-24 14:47:50 +0300

    score method done

--- src/main/java/UI/Controllers/ScoreController.java
+++ src/main/java/UI/Controllers/ScoreController.java
@@ -3,6 +3,7 @@ package UI.Controllers;
 import UI.SceneLoaders.PageLoader;
 import UI.SceneLoaders.PopUpLoader;
 import UI.SceneLoaders.page_select;
+import benchmark.OpenGL.Generator;
 import database.DBUtils_local;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -10,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 import java.io.IOException;
 import java.net.URL;
@@ -18,6 +20,8 @@ import java.util.ResourceBundle;
 public class  ScoreController implements Initializable {
 
     @FXML
+    private GridPane g_information;
+    @FXML
     private Label l_name;
 
     @FXML
@@ -25,7 +29,7 @@ public class  ScoreController implements Initializable {
 
     @FXML
     private Label l_color;
-
+    public Generator generator;
     @FXML
     private Label l_score;
 
@@ -41,6 +45,18 @@ public class  ScoreController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         DBUtils_local.score(null, l_name.getText(), Integer.parseInt(l_input.getText()), l_color.getText(), Float.parseFloat(l_score.getText()));
+
+
+        /**public void setValueInGridPane() {
+             l_score = new Label(String.valueOf(value)); // Create a label with the value
+
+            // Set the position of the label in the GridPane
+            GridPane.setRowIndex(label, row);
+            GridPane.setColumnIndex(label, column);
+
+            // Add the label to the GridPane
+            g_information.getChildren().add(l_score);
+        }*/
 
         b_hist.setOnAction(new EventHandler<ActionEvent>() {
 
