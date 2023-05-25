2cd9bb645aa380989945ed4cd3351ed3538da175
Cristina <cristina.rodean123@gamil.com>
2023-05-25 07:59:46 +0300

    fxml choose test

---
+++ src/main/java/UI/Controllers/ChooseTestController.java
@@ -0,0 +1,13 @@
+package UI.Controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChooseTestController implements Initializable, IController{
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
